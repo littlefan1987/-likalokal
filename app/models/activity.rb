8d6aa9ef2263e8459c306346fd094f030e05479b
Aurelie Mahe <mahe.aureliems@gmail.com>
2019-08-19 17:31:07 +0200

    fix error

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,7 +2,7 @@ class Activity < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
 
-  validates :name, presence: true,
+  validates :name, presence: true
   validates :description, presence: true
   validates :price, presence: true
   validates :city, presence: true
