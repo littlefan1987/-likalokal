85218664b049d5c3bd52ce1587e6818b3776224d
Aurelie Mahe <mahe.aureliems@gmail.com>
2019-08-20 11:30:48 +0200

    add myactivities and update method

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,2 +1,34 @@
 class ActivitiesController < ApplicationController
+  before_action :set_activity, only: [:edit, :update, :destroy]
+
+  def index
+    @activities = Activity.all
+  end
+
+  def myactivities
+    @activity = current_user.activities.order(created_at: :desc)
+  end
+
+  def edit
+  end
+
+  def update
+    @activities = current_user.activities.order(created_at: :desc)
+    activity = @activities.find(params[:id])
+    if activity.update(activity_params)
+      redirect_to myactivities_path, notice: "Your activity was successfully updated"
+    else
+      render :myactivities
+    end
+  end
+
+  private
+
+  def set_activity
+    @activity = Activity.find(params[:id])
+  end
+
+  def activity_params
+    params.require(:activity).permit(:name, :description, :price, :city, :photo, :category)
+  end
 end
