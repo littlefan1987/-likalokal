777c46bf93f4597a10c12ecd101b2345c3bc7e69
caroline debeve <c.debeve@gmail.com>
2019-08-20 12:49:53 +0200

    create activity new

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,2 +1,21 @@
 class ActivitiesController < ApplicationController
+  def new
+    @activity = Activity.new
+  end
+
+  def create
+    @activity = Activity.new(activity_params)
+    @activity.user = current_user
+    if @activity.save
+      redirect_to guide_dashboard_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def activity_params
+    params.require(:activity).permit(:name, :description, :price, :city, :category)
+  end
 end
