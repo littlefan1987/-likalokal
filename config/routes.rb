2c81d8f62b965bdab3550d5c4abae152ecc8e247
Julien VENNIN <julien.vennin@gmail.com>
2019-08-21 12:39:14 +0200

    Feature show my activities + insert form bookin in show activity

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,13 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'user/travellers/dashboard', to: 'bookings#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :activities, only: [:new, :create, :index, :show, :edit] do
+  resources :activities, only: [:new, :create, :index, :show, :edit, :destroy] do
     resources :bookings, only: [:new, :create]
   end
   resources :bookings, only: [:index, :show, :edit, :destroy]
   get "guide_dashboard", to: "users#guide_dashboard"
+
+  get "myactivities", to: "activities#myactivities"
 
   get "traveller_dashboard", to: "users#traveller_dashboard"
   patch "activities/:id", to: "activities#update"
