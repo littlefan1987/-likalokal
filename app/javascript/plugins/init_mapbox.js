d0e1e86dec45861f97e9de850f1a0f8d363d9b14
Julien VENNIN <julien.vennin@gmail.com>
2019-08-22 18:08:03 +0200

    add footer + fix bug js select2 map

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,13 +1,13 @@
 import mapboxgl from 'mapbox-gl';
 
-const initMapbox = () => {
-  const mapElement = document.getElementById('map');
-
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
   map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
 };
+
+const initMapbox = () => {
+  const mapElement = document.getElementById('map');
 
   if (mapElement) { // only build a map if there's a div#map to inject into
     mapboxgl.accessToken = "pk.eyJ1IjoiY2Fyb2xpbmVubCIsImEiOiJjanpsM2d4eDIwMWQ3M2NwZjM5OXI0c290In0.mc_kUQ31Wrh8NzJumRqTiA";
