43145d855964ac926b65a35b7b746de0823457a9
Julien VENNIN <julien.vennin@gmail.com>
2019-08-22 16:04:00 +0200

    try search

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
-
   def home
     @activities = Activity.all
+    if params[:city].present?
+      @activities = Activity.where(params[:city])
+    else
+      @movies = Activity.all
+    end
   end
 end
