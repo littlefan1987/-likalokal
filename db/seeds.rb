8f0a46355a6ba35582c45246af442b27fad80da9
caroline debeve <c.debeve@gmail.com>
2019-08-23 13:41:53 +0200

    traveller dashboard navbar et seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,68 +13,6 @@ p = "password123"
 
 puts "Creating activities dark and full of terrors"
 
-Activity.create(name: "Satanism and Devil Worshiping",
-  description: "If the Age of the Enlightenment and its philosophers has been greatly influenced by French authors, the XIX century marks the return of the devil. Even Victor Hugo had gladly accepted the title of president of the Satanic Temple of France, French press had awarded him.",
-  price: "25",
-  address: "Place Saint-Michel, Paris",
-  photo: "../images/satanism.jpg",
-  category: "Occult & Esotericism")
-
-Activity.create(name: "Dublin - Ghosts, Vampires and Serial Killers",
-  description: "From sinister surgeons and bodysnatchers to ghosts and ghouls... Discover mythological villains and real ones too, buried deep in Dublin's gruesome past. Visit a medieval vault a hidden city graveyard. It's a night tour and there's quite a bit of walking involved, so put on your sneakers. Besides heels aren't practical when running away from werewolves.",
-  price: "25",
-  address: "Christchurch, Dublin",
-  photo: "dublin_bus.jpg",
-  category: "Occult & Esotericism")
-
-Activity.create(name: "Death, Life and Symbolism",
-  description: "When visiting the Père Lachaise Cemetery, you get a sense that the bourgeoisie, and even the bohemians of the day – were competing with each other over who’s got the fanciest eternal resting spot. Prepare yourself for a visit like no other in town and drop by to say hello to some of the most influential people of their time (and even of our time). So are you ready to explore the Père Lachaise Cemetery?",
-  price: "20",
-  address: "Père Lachaise, Paris",
-  photo: "../images/pere_lachaise",
-  category: "Culture & History")
-
-Activity.create(name: "Zombie Hunt",
-  description: "History shows that this idea dates back to the 8th century. The word 'zombie' is said to have come from nzambi, which in Kongo means 'spirit of a dead person', or zonbi, used in the Louisiana Creole or the Haitian Creole that represents a person who died and was then brought to life without speech or free will. If you're ready for the hunt, be careful not to become the prey.",
-  price: "12",
-  address: "Louisiana",
-  photo: "../images/zombie_hunt.jpg",
-  category: "Occult & Esotericism")
-
-Activity.create(name: "Ghosts of Montmartre",
-  description: "Mont de Mars - the soon-to-become Mont des Martyrs where Saint-Denis, decapitated, would hold his severed head and haunt the streets of the hill. Follow me for a visit hosted by the phantoms - famous or not, of the hillock. Relive the tragedy of Montmartre's windmills, the insanity of its artists (Van Gogh, Nerval...) or the invasion of Place du Tertre.",
-  price: "20",
-  address: "Montmartre, Paris",
-  photo: "../images/ghost_montmartre.jpg",
-  category: "Occult & Esotericism")
-
-Activity.create(name: "Sedlec Ossuary",
-  description: "The Sedlec Ossuary is nothing spectacular in the outside. It is a small chapel located in Sedlec, in the suburbs of Kutna Hora, in the Czech Republic. You would think that it is just an average old medieval gothic church. As you enter the Sedlec Ossuary though, you will soon realize why it is one of the most amazing and unique churches in the world.The Sedlec Ossuary is artistically decorated by more than 40.000 human skeletons.",
-  price: "20",
-  address: "Kutná Hora",
-  photo: "../images/bone_chapel.jpg",
-  category: "Culture & History")
-
-Activity.create(name: "History & Mystery in Edinburgh’s Closes",
-  description: "As you walk up the Royal Mile, it’s hard to miss the arched entryways that sit on either side. Signposted with odd names like ‘Fleshmarket’, ‘Lady Stair’s’ and ‘World’s End’, these closes, wynds, courts and entries play an essential role in Edinburgh’s charming infrastructure. Situated within the (former) town walls, the exceptionally narrow streets weren’t a mistake of engineering. With the threat of English attack always at the forefront, skinny streets surrounded by tall buildings provided early city dwellers with protection and peace of mind. Edinburgh’s medieval honeycomb of hidden alleyways also contains some of the city’s best stories, secrets and mysteries. Many of Edinburgh’s most influential characters once called these closes home – and some are believed to still lurk among the cobblestones.",
-  price: "10",
-  address: "High Street, Edinburgh",
-  photo: "../images/edinburgh.jpg",
-  category: "Culture & History")
-
-Activity.create(name: "Dark Heart & Murder Mysteries",
-  description: "Uncover gruesome secrets from the city of Lights' dark past. Learn about public executions, ritual worship and the devastations of the plague. I'll narrate chilling stories that are bound to give you a fright - nothing that can't be solved by a glass of wine though.",
-  price: "20",
-  address: "Kutná Hora",
-  photo: "../images/bone_chapel.jpg",
-  category: "True Crime")
-
-Activity.create(name: "In the Footsteps of Jack The Ripper",
-  description: "As the night falls, and the long shadows reach into the darker recesses of the streets of Whitechapel and Spitalfields, our Original Jack the Ripper Crime Scene Investigation sets out  to join the Victorian police as they hunt history’s most infamous serial killer through the crooked, cobbled alleyways of the Victorian abyss. Step by blood-curdling step, you will find yourself spirited back to the mean streets of the 19th century East End where every corner turned and every step taken will lead you further in to a murder mystery that has been baffling and terrifying people for more than 125 years.",
-  price: "35",
-  address: "London",
-  photo: "../images/the_ripper.jpg",
-  category: "True Crime")
 
 # users
 
@@ -100,7 +38,7 @@ User.create(
     first_name: "Victor",
     email: "v.krum@gmail.com",
     password: p,
-    photo: "https://source.unsplash.com/QCPXRvl9Ylc/",
+    avatar: "https://source.unsplash.com/QCPXRvl9Ylc/",
     description: "As a student in Medieval History, Paris is more than a home for me: it's a teacher, a lover, a mystery to be relentlessly discovered."
   )
 
@@ -108,7 +46,7 @@ User.create(
     first_name: "George",
     email: "g.weasley@gmail.com",
     password: p,
-    photo: "https://source.unsplash.com/vMV6r4VRhJ8/",
+    avatar: "https://source.unsplash.com/vMV6r4VRhJ8/",
     description: "A good laugh heals a lot of hurts. A little bit of thrill can't hurt either"
   )
 
@@ -116,7 +54,7 @@ User.create(
     first_name: "Severus",
     email: "s.snape@gmail.com",
     password: p,
-    photo: "https://source.unsplash.com/SZ9y9PGVQbY/",
+    avatar: "https://source.unsplash.com/SZ9y9PGVQbY/",
     description: "Always"
   )
 
@@ -124,7 +62,7 @@ User.create(
     first_name: "Fenrir",
     email: "f.greyback@gmail.com",
     password: p,
-    photo: "https://source.unsplash.com/aWQhVALvDZ4/",
+    avatar: "https://source.unsplash.com/aWQhVALvDZ4/",
     description: "I belong to the darkness. I'll show you the darker side of the City of Lights, full of tortures, witchcraft and crimes"
   )
 
@@ -132,7 +70,7 @@ User.create(
     first_name: "Neuville",
     email: "n.longbottom@gmail.com",
     password: p,
-    photo: "https://source.unsplash.com/i4OHxtxiMtk",
+    avatar: "https://source.unsplash.com/i4OHxtxiMtk",
     description: "Artist by night and by day, street art and urban culture are my jam"
   )
 
@@ -140,8 +78,81 @@ User.create(
     first_name: "Cedric",
     email: "c.diggory@gmail.com",
     password: p,
-    photo: "https://source.unsplash.com/iJVDkgWqp3U",
+    avatar: "https://source.unsplash.com/iJVDkgWqp3U",
     description: "I was born a guitar a hand and violon in the other. Wether you're looking for the jazziest jam session, a more classical concerto evening or trying to learn accordion - I'm your guy."
   )
+
+
+Activity.create!(name: "Satanism and Devil Worshiping",
+  description: "If the Age of the Enlightenment and its philosophers has been greatly influenced by French authors, the XIX century marks the return of the devil. Even Victor Hugo had gladly accepted the title of president of the Satanic Temple of France, French press had awarded him.",
+  price: "25",
+  address: "Place Saint-Michel, Paris",
+  photo: "../images/satanism.jpg",
+  category: "Occult & Esotericism",
+  user: User.first)
+
+Activity.create!(name: "Dublin - Ghosts, Vampires and Serial Killers",
+  description: "From sinister surgeons and bodysnatchers to ghosts and ghouls... Discover mythological villains and real ones too, buried deep in Dublin's gruesome past. Visit a medieval vault a hidden city graveyard. It's a night tour and there's quite a bit of walking involved, so put on your sneakers. Besides heels aren't practical when running away from werewolves.",
+  price: "25",
+  address: "Christchurch, Dublin",
+  photo: "dublin_bus.jpg",
+  category: "Occult & Esotericism",
+  user: User.first)
+
+Activity.create!(name: "Death, Life and Symbolism",
+  description: "When visiting the Père Lachaise Cemetery, you get a sense that the bourgeoisie, and even the bohemians of the day – were competing with each other over who's got the fanciest eternal resting spot. Prepare yourself for a visit like no other in town and drop by to say hello to some of the most influential people of their time (and even of our time). So are you ready to explore the Père Lachaise Cemetery?",
+  price: "20",
+  address: "Père Lachaise, Paris",
+  photo: "../images/pere_lachaise",
+  category: "Culture & History",
+  user: User.first)
+
+Activity.create!(name: "Zombie Hunt",
+  description: "History shows that this idea dates back to the 8th century. The word 'zombie' is said to have come from nzambi, which in Kongo means 'spirit of a dead person', or zonbi, used in the Louisiana Creole or the Haitian Creole that represents a person who died and was then brought to life without speech or free will. If you're ready for the hunt, be careful not to become the prey.",
+  price: "12",
+  address: "Louisiana",
+  photo: "../images/zombie_hunt.jpg",
+  category: "Occult & Esotericism",
+  user: User.first)
+
+Activity.create!(name: "Ghosts of Montmartre",
+  description: "Mont de Mars - the soon-to-become Mont des Martyrs where Saint-Denis, decapitated, would hold his severed head and haunt the streets of the hill. Follow me for a visit hosted by the phantoms - famous or not, of the hillock. Relive the tragedy of Montmartre's windmills, the insanity of its artists (Van Gogh, Nerval...) or the invasion of Place du Tertre.",
+  price: "20",
+  address: "Montmartre, Paris",
+  photo: "../images/ghost_montmartre.jpg",
+  category: "Occult & Esotericism",
+  user: User.first)
+
+Activity.create!(name: "Sedlec Ossuary",
+  description: "The Sedlec Ossuary is nothing spectacular in the outside. It is a small chapel located in Sedlec, in the suburbs of Kutna Hora, in the Czech Republic. You would think that it is just an average old medieval gothic church. As you enter the Sedlec Ossuary though, you will soon realize why it is one of the most amazing and unique churches in the world.The Sedlec Ossuary is artistically decorated by more than 40.000 human skeletons.",
+  price: "20",
+  address: "Kutná Hora",
+  photo: "../images/bone_chapel.jpg",
+  category: "Culture & History",
+  user: User.first)
+
+Activity.create!(name: "History & Mystery in Edinburgh's Closes",
+  description: "As you walk up the Royal Mile, it's hard to miss the arched entryways that sit on either side. Signposted with odd names like 'Fleshmarket', 'Lady Stair's' and 'World's End', these closes, wynds, courts and entries play an essential role in Edinburgh's charming infrastructure. Situated within the (former) town walls, the exceptionally narrow streets weren't a mistake of engineering. With the threat of English attack always at the forefront, skinny streets surrounded by tall buildings provided early city dwellers with protection and peace of mind. Edinburgh's medieval honeycomb of hidden alleyways also contains some of the city's best stories, secrets and mysteries. Many of Edinburgh's most influential characters once called these closes home – and some are believed to still lurk among the cobblestones.",
+  price: "10",
+  address: "High Street, Edinburgh",
+  photo: "../images/edinburgh.jpg",
+  category: "Culture & History",
+  user: User.first)
+
+Activity.create!(name: "Dark Heart & Murder Mysteries",
+  description: "Uncover gruesome secrets from the city of Lights' dark past. Learn about public executions, ritual worship and the devastations of the plague. I'll narrate chilling stories that are bound to give you a fright - nothing that can't be solved by a glass of wine though.",
+  price: "20",
+  address: "Kutná Hora",
+  photo: "../images/bone_chapel.jpg",
+  category: "True Crime",
+  user: User.first)
+
+Activity.create!(name: "In the Footsteps of Jack The Ripper",
+  description: "As the night falls, and the long shadows reach into the darker recesses of the streets of Whitechapel and Spitalfields, our Original Jack the Ripper Crime Scene Investigation sets out  to join the Victorian police as they hunt history's most infamous serial killer through the crooked, cobbled alleyways of the Victorian abyss. Step by blood-curdling step, you will find yourself spirited back to the mean streets of the 19th century East End where every corner turned and every step taken will lead you further in to a murder mystery that has been baffling and terrifying people for more than 125 years.",
+  price: "35",
+  address: "London",
+  photo: "../images/the_ripper.jpg",
+  category: "True Crime",
+  user: User.first)
 
 
