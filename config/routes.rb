9aac73023c2a54f4722d96e61b762010e117d219
Julien VENNIN <julien.vennin@gmail.com>
2019-08-23 09:19:56 +0200

    Improve design

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-
+  get 'user/travellers/dashboard', to: 'bookings#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :activities, only: [:new, :create, :index, :show, :edit, :destroy] do
@@ -9,7 +9,8 @@ Rails.application.routes.draw do
   end
   resources :bookings, only: [:index, :show, :edit, :destroy]
   get "guide_dashboard", to: "users#guide_dashboard", as: :guide_dashboard
-
+  # temporaire to delete ci dessous
+  get 'user/travellers/dashboard', to: 'bookings#index'
 
   get "myactivities", to: "activities#myactivities"
 
