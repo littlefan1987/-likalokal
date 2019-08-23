609096d8b76485cd847b87f5d340fc169da4a9b4
Aurelie Mahe <mahe.aureliems@gmail.com>
2019-08-23 16:27:00 +0200

    change visuals

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,14 @@ class ApplicationController < ActionController::Base
   before_action :store_user_location!, if: :storable_location?
   before_action :authenticate_user!, except: [:home]
 
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :description])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :description])
+  end
+
   private
   # Its important that the location is NOT stored if:
   # - The request method is not GET (non idempotent)
